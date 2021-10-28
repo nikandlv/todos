8619d0cc040f66d1e067999ad840a5d3e2f0d73f
Nikan Dalvand <nikandalvand@gmail.com>
2021-10-28 22:36:58 +0330

    Updates

---
+++ static/js/main.6da63016.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptodos=this.webpackJsonptodos||[]).push([[0],{9:function(t,e,n){"use strict";n.r(e);var o=n(1),c=n.n(o),i=n(3),r=n.n(i),s=n(0);function d(){return Object(s.jsx)("div",{children:"Hello!"})}var u=function(t){t&&t instanceof Function&&n.e(3).then(n.bind(null,10)).then((function(e){var n=e.getCLS,o=e.getFID,c=e.getFCP,i=e.getLCP,r=e.getTTFB;n(t),o(t),c(t),i(t),r(t)}))};r.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(d,{})}),document.getElementById("root")),u()}},[[9,1,2]]]);
+//# sourceMappingURL=main.6da63016.chunk.js.map

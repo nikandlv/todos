761f526b2bccbf527dd8b22448bd079d3169ac1a
Nikan Dalvand <nikandalvand@gmail.com>
2021-10-28 22:44:27 +0330

    Added mock data and todo types

---
+++ src/app/Data/Todos.ts
@@ -0,0 +1,20 @@
+import { TodoList } from 'app/Types/Todo'
+const Todos: TodoList = [
+    {
+        id: 1,
+        title: 'First example',
+        checked: false,
+    },
+    {
+        id: 2,
+        title: 'Second example',
+        checked: true,
+    },
+    {
+        id: 3,
+        title: 'Third example',
+        checked: false,
+    },
+]
+
+export default Todos
